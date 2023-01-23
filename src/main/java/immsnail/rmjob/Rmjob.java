9257a9574bfedd7e3d884d9b34b4ee4dad5fdae0
Notarin <424c414e4b@gmail.com>
2023-01-22 22:33:42 -0500

    Added a startup message
    "RMJob has started!"

--- src/main/java/immsnail/rmjob/Rmjob.java
+++ src/main/java/immsnail/rmjob/Rmjob.java
@@ -2,9 +2,11 @@ package immsnail.rmjob;
 
 import net.fabricmc.api.ModInitializer;
 
+import static immsnail.rmjob.LogHandler.logInfo;
+
 public class Rmjob implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        logInfo("RMJob has started!");
     }
 }
