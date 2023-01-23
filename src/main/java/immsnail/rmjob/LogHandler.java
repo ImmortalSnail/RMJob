d86bf4839a5581873f2eb9222384c9ab66090d31
Notarin <424c414e4b@gmail.com>
2023-01-22 22:31:14 -0500

    Created LogHandler class
    Moved the LOGGER to a class with four methods for the varying log levels: logInfo(), logDebug(), logError(), and logWarn().

---
+++ src/main/java/immsnail/rmjob/LogHandler.java
@@ -0,0 +1,56 @@
+package immsnail.rmjob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
+public class LogHandler {
+
+    /**
+     * This logger is used to write text to the console and the log file.
+     * It is considered best practice to use your mod id as the logger's name.
+     * That way, it's clear which mod wrote info, warnings, and errors.
+     */
+    public static final Logger LOGGER = LoggerFactory.getLogger("RMJob");
+
+    /**
+     * This method is used to log informative messages that provide general
+     * information about the application's current state or activity.
+     * @param string - the message to be logged
+     */
+    public static void logInfo(String string) {
+        LOGGER.info(string);
+    }
+
+    /**
+     * This method is used to log debug messages that provide detailed
+     * information about the application's inner workings, such as variable
+     * values or function calls, for the purpose of troubleshooting and
+     * resolving errors.
+     * @param string - the message to be logged
+     */
+    public static void logDebug(String string) {
+        LOGGER.debug(string);
+    }
+
+    /**
+     * This method is used to log error messages that indicate a failure or
+     * unexpected behavior in the application, along with information that
+     * can help identify and fix the issue.
+     * @param string - the message to be logged
+     */
+    public static void logError(String string) {
+        LOGGER.error(string);
+    }
+
+    /**
+     * This method is used to log warning messages that indicate a potential 
+     * problem or situation that requires attention, but does not necessarily 
+     * prevent the application from functioning.
+     * @param string - the message to be logged
+     */
+    public static void logWarn(String string) {
+        LOGGER.warn(string);
+    }
+
+}
--- src/main/java/immsnail/rmjob/client/RmjobClient.java
+++ src/main/java/immsnail/rmjob/client/RmjobClient.java
@@ -1,8 +1,6 @@
 package immsnail.rmjob.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The main client class for RMJob, this class handles mod startup for
@@ -10,13 +8,6 @@ import org.slf4j.LoggerFactory;
  */
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class RmjobClient implements ClientModInitializer {
-
-    /**
-     * This logger is used to write text to the console and the log file.
-     * It is considered best practice to use your mod id as the logger's name.
-     * That way, it's clear which mod wrote info, warnings, and errors.
-     */
-    public static final Logger LOGGER = LoggerFactory.getLogger("RMJob");
 
     /**
      * This code runs as soon as Minecraft is in a mod-load-ready state.
